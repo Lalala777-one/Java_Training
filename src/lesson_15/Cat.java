81d54bf203f75f3f13b8f3d07060c7dd47e1c8bf
Anastasiia Yemets <anastasiaemec40@gmail.com>
2024-10-01 09:26:40 +0200

    lesson 15 and homework 15

---
+++ src/lesson_15/Cat.java
@@ -0,0 +1,49 @@
+package lesson_15;
+
+public class Cat {
+
+    String name;
+    String color;
+    int age;
+
+    // КОНСТРУКТОР КЛАССА
+   // специальный тип метода, основная задача - установить начальные значения свойств объекта
+/*
+1. Имя конструктора совпадает с именем класса
+2. Конструктор не имеет возвращаемого типа
+3. Конструктор вызывается автоматически при создании объекта.
+ */
+
+    public Cat(String name, String color, int age){
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    };
+
+
+    public Cat(String name){
+        this.name = name;
+    }
+
+    public Cat (String name, String color){
+        this.name = name;
+        this.color = color;
+    }
+
+
+    public void sleep(){
+        System.out.println("I am sleeping");
+    }
+
+    public void run(){
+        System.out.println("I am running");
+    }
+
+    public void sayMeow(){
+        System.out.println("Meow");
+    }
+
+    public void whoAmI(){
+        System.out.printf("Я котик %s, мой возраст: %d, мой окрас: %s\n", name, age, color);
+    }
+}
